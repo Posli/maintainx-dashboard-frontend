20df3fb5330ab3a9ed9141b7b85ff23b563fb784
Posli <144464286+Posli@users.noreply.github.com>
2025-10-31 16:46:35 -0500

    Add Asset interface to index.d.ts

---
+++ src/types/index.d.ts
@@ -0,0 +1,8 @@
+// Puedes extender tipos de MaintainX o Supabase aquí
+export interface Asset {
+  id: string;
+  name: string;
+  serial_number: string;
+  location_id: string;
+  vendor_id: string;
+}
